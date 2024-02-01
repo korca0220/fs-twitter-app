7340ad9fb1336c1677ece8a786cd6cab6310017b
ParkJuneWoo <krca0220@gmail.com>
2024-02-01 23:09:04 +0900

    a

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,3 +1,68 @@
+import PostBox from "components/posts/PostBox";
+import AuthContext from "context/AuthContext";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
+import { PostProps } from "pages/home";
+import { useContext, useEffect, useState } from "react";
+
 export default function SearchPage() {
-  return <h1>Search Page</h1>;
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const [tagQuery, setTagQuery] = useState<string>("");
+  const { user } = useContext(AuthContext);
+
+  const onChange = (e: any) => {
+     (e?.target?.value.trim());
+  };
+
+  useEffect(() => {
+    if (user) {
+      const postRef = collection(db, "posts");
+      const postQuery = query(
+        postRef,
+        where("hashTag", "array-contains-any", [tagQuery]),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postQuery, (snapshot) => {
+        const dataObj = snapshot?.docs?.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [tagQuery, user]);
+
+  return (
+    <div className="home">
+      <div className="home__tap">
+        <div className="home__title">
+          <div className="home__title-text">Search</div>
+        </div>
+        <div className="home__search-div">
+          <input
+            className="home__search"
+            placeholder="해시태그 검색"
+            onChange={onChange}
+          ></input>
+        </div>
+      </div>
+      <div className="post">
+        {posts?.length > 0 ? (
+          posts?.map((post) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="post__text">게시글이 없습니다.</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
