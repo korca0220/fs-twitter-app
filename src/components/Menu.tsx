694b057d2c6b42e996cb1ced866d5b71520e30ca
ParkJuneWoo <krca0220@gmail.com>
2024-02-25 22:29:18 +0900

    add vercel

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -38,29 +38,29 @@ export default function MenuList() {
       <div className="footer__grid">
         <button type="button" onClick={() => navigate("/")}>
           <BsHouse />
-          {t("MENU_HOME")}
+          <span className="footer__grid--text"> {t("MENU_HOME")}</span>
         </button>
         <button type="button" onClick={() => navigate("/profile")}>
           <BiUserCircle />
-          {t("MENU_PROFILE")}
+          <span className="footer__grid--text">{t("MENU_PROFILE")}</span>
         </button>
         <button type="button" onClick={() => navigate("/search")}>
           <AiOutlineSearch />
-          {t("MENU_SEARCH")}
+          <span className="footer__grid--text">{t("MENU_SEARCH")}</span>
         </button>
         <button type="button" onClick={() => navigate("/Notifications")}>
           <IoMdNotificationsOutline />
-          {t("MENU_NOTI")}
+          <span className="footer__grid--text">{t("MENU_NOTI")}</span>
         </button>
         {user === null ? (
           <button type="button" onClick={() => navigate("/user/login")}>
             <MdLogin />
-            {t("MENU_LOGIN")}
+            <span className="footer__grid--text">{t("MENU_LOGIN")}</span>
           </button>
         ) : (
           <button type="button" onClick={handleClickLogout}>
             <MdLogout />
-            {t("MENU_LOGOUT")}
+            <span className="footer__grid--text">{t("MENU_LOGOUT")}</span>
           </button>
         )}
       </div>
